00b95e6b34e675590909a3b00731aea8b188ae66
Misha <mishanya_1973user@mail.ru>
2025-03-03 11:55:55 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -2,6 +2,7 @@ print("Hello world!")
 print('*' * 50)
 for i in range(1, 50):
   if i % 2 == 0:
-    print(i ** 2)
+    number = int(input('Введите число'))
+    print(f'Результат: {i * number}')
   else:
     continue
